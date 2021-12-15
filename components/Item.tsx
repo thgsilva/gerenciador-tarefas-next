ffe2de38cf034d2280371ad9c4a236eeaf9ae130
Thiago <thiago.giannolli@hotmail.com>
2021-12-14 23:17:05 -0300

    versao ajustada

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,13 +1,13 @@
-
 import moment from "moment";
 import { NextPage } from "next";
 import { Task } from "../types/Task";
 
 type ItemProps = {
     task : Task
+    selectTask(task : Task) : void
 }
 
-export const Item : NextPage<ItemProps> = ({ task }) => {
+export const Item : NextPage<ItemProps> = ({ task, selectTask }) => {
 
     const getDateText = (task : Task) => {
         if(task && task.finishDate){
@@ -18,11 +18,12 @@ export const Item : NextPage<ItemProps> = ({ task }) => {
     }
 
     return (
-        <div className="container-item">
+        <div className={"container-item" + (task?.finishDate ? "" : " active")}
+            onClick={e => task?.finishDate ? null : selectTask(task)}>
             <img src={task?.finishDate ? '/checked.svg' : 'not-checked.svg'}
                 alt={task?.finishDate ? 'Tarefa Concluída' : 'Tarefa não concluída'}/>
             <div>
-                <p>{task.name}</p>
+                <p className={task?.finishDate ? "finished" : ""}>{task.name}</p>
                 <span>{getDateText(task)}</span>
             </div>
         </div>
--- containers/Home.tsx
+++ containers/Home.tsx
@@ -1,4 +1,3 @@
-
 import { NextPage } from "next";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -7,17 +6,19 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { List } from "../components/List";
 import { executeRequest } from "../services/api";
-
+import { Task } from "../types/Task";
+import {CrudModal} from '../components/Modal';
 type HomeProps = {
     setToken(s: string) : void
 }
 
 export const Home : NextPage<HomeProps> = ({setToken}) => {
 
+    // state Filter
     const [previsionDateStart, setPrevisionDateStart] = useState('');
     const [previsionDateEnd, setPrevisionDateEnd] = useState('');
     const [status, setStatus] = useState('0');
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
     const sair = () =>{
         localStorage.removeItem('accessToken');
@@ -39,7 +40,7 @@ export const Home : NextPage<HomeProps> = ({setToken}) => {
 
             const result = await executeRequest('task'+filter, 'GET');
             if(result && result.data){
-                setTasks(result.data);
+                setTasks(result.data as Task[]);
             }
         }catch(e){
             console.log(e);
@@ -50,9 +51,49 @@ export const Home : NextPage<HomeProps> = ({setToken}) => {
         getFilteredList();
     }, [status, previsionDateStart, previsionDateEnd]);
 
+
+    // state Modal
+    const [showModal, setShowModal] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
+    const [name, setName] = useState('');
+    const [previsionDate, setPrevisionDate] = useState('');
+
+    const closeModal = () => {
+        setShowModal(false);
+        setErrorMsg('');
+        setName('');
+        setPrevisionDate('');
+    }
+    
+    const doSave = async () => {
+        try{
+            if(!name || !previsionDate){
+                setErrorMsg('Favor preencher nome e data de previsão');
+                return;
+            }
+
+            const body = {
+                name,
+                previsionDate
+            }
+
+            await executeRequest('task', 'POST', body);
+            await getFilteredList();
+            closeModal();
+        }catch(e){
+            if(e?.response?.data?.error){
+                console.log(e?.response);
+                setErrorMsg(e?.response?.data?.error);
+                return;
+            }
+            console.log(e);
+            setErrorMsg('Ocorreu erro ao cadastrar tarefa, tente novamenete');
+        }
+    }
+
     return (
     <>
-        <Header sair={sair}/>
+        <Header sair={sair} showModal={() => setShowModal(true)}/>
         <Filter 
             previsionDateStart={previsionDateStart}
             previsionDateEnd={previsionDateEnd}
@@ -61,7 +102,17 @@ export const Home : NextPage<HomeProps> = ({setToken}) => {
             setPrevisionDateEnd={setPrevisionDateEnd}
             setStatus={setStatus}
         />
-        <List tasks={tasks}/>
-        <Footer />
+        <List tasks={tasks} getFilteredList={getFilteredList}/>
+        <Footer showModal={() => setShowModal(true)}/>
+        <CrudModal 
+            showModal={showModal}
+            errorMsg={errorMsg}
+            name={name}
+            setName={setName}
+            previsionDate={previsionDate}
+            setPrevisionDate={setPrevisionDate}
+            closeModal={closeModal}
+            doSave={doSave}
+        />
     </>);
 }
